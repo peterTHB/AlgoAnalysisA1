48f160ef6b332cfd0ee583b03f196af413ce2f5a
Peter Bui s3786794 <s3786794@student.rmit.edu.au>
2020-08-23 16:29:02 +1000

    Finished list class implementation

--- implementation/ListClass.java
+++ implementation/ListClass.java
@@ -5,67 +5,75 @@ import java.util.*;
 @SuppressWarnings("serial")
 public class ListClass<Type> extends LinkedList<Type> implements Iterable<Type>
 {
-	private Node current;
+	private Node head;
 	
 	public ListClass(String[] array, int count) {
+		head = new Node(null, null);
 		addArray(array, count);
 	}
 	
 	public ListClass(Node node, int count, int check) {
+		head = new Node(null, null);
+		Node current = head;
 		if (check == 0) {
-			addBST(node, count);
+			addBST(current, node, count);
 		} else {
-			addNode(node, count);
+			addNode(current, node, count);
 		}
 	}
 	
 	private void addArray(String[] array, int count) {
+		Node current = head;
+
 		for (int i = 0; i < array.length; i++) {
 			String[] currString = array[i].split(":");
-					
-			if (Integer.parseInt(currString[1]) == count) {
-				current = new Node(array[i], null);
-				current.getNext();
-			}
-		}
-	}
-	
-	private void addNode(Node node, int count) {
-		while (node != null) {
-			String[] currString = node.getData().split(":");
 			
 			if (Integer.parseInt(currString[1]) == count) {
-				current = new Node(node.getData(), null);
+				current.setData(array[i].split(":")[0]);
+				current.setNext(new Node(null, null));
 				current = current.getNext();
 			}
 		}
 	}
 	
-	private void addBST(Node node, int count) {
-		if (node != null) {
-			String[] currString = node.getData().split(":");
+	private void addNode(Node current, Node enter, int count) {
+		while (enter.getNext() != null) {
+			String[] currString = enter.getData().split(":");
+			
 			if (Integer.parseInt(currString[1]) == count) {
-				current = new Node(node.getData(), null);
+				current.setData(enter.getData().split(":")[0]);
+				current.setNext(new Node(null, null));
 				current = current.getNext();
 			}
-			addBST(node.getLeft(), count);
-			addBST(node.getRight(), count);
+			enter = enter.getNext();
+		}
+	}
+	
+	private void addBST(Node current, Node enter, int count) {
+		if (enter != null) {
+			String[] currString = enter.getData().split(":");
+			if (Integer.parseInt(currString[1]) == count) {
+				current.setData(enter.getData().split(":")[0]);
+				
+				current.setNext(new Node(null, null));
+				
+				current = current.getNext();
+				
+				addBST(current, enter.getLeft(), count);
+				addBST(current, enter.getRight(), count);
+			}
 		}
 	}
 	
 	public Iterator<Type> iterator() {
 		Iterator<Type> it = new Iterator<Type>() {
 
+			Node current = head;
+			
 			@Override
 			public boolean hasNext()
 			{
-				boolean check = false;
-				
-				if (current != null) {
-					check = true;
-				}
-				
-				return check;
+				return current.getNext() != null;
 			}
 
 			@SuppressWarnings("unchecked")
@@ -75,7 +83,8 @@ public class ListClass<Type> extends LinkedList<Type> implements Iterable<Type>
 				Type check = null;
 				
 				if (hasNext()) {
-					check = (Type) current.getNext().getData();
+					check = (Type) current.getData();
+					current = current.getNext();
 				}
 				
 				return check;
